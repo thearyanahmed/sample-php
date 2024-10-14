f7b1c636d1c0017bc370e00cdb74b1853d0ee381
aryan <paryan@digitalocean.com>
2024-10-14 10:21:19 +0200

    Add sleep for SLOW

--- index.php
+++ index.php
@@ -10,5 +10,10 @@ if(isset($_GET['message']) && $_GET['message'] != '') {
 	$text = htmlspecialchars($_GET['message']);
 }
 
+if(isset($_GET['SLOW']) && $_GET['SLOW'] != '') {
+	sleep(4);
+	$text = "I'm a slow cow";
+}
+
 $cow = Farm::create(\Cowsayphp\Farm\Cow::class);
 echo $cow->say($text);
